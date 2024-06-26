0f7db5bbc797ff60b6d15c3e7c86b8ceef771b6f
Sajith <sajithanuradha860@gmail.com>
2024-06-26 10:40:55 +0530

    default home page UI finished

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/Others/addPost/addPhotoHome.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Addphotohome(key: Key('addPhotoHome')),
     );
   }
 }
