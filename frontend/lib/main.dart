879000478afb85fdd686898692f1ec6fbb73b9ab
Verosha Kriyanjala <veroshakriyanjala32@gmail.com>
2024-06-27 12:08:28 +0530

    Completed Explore section

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/Explore/explore.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Explore(key: const Key('explore')),
     );
   }
 }
