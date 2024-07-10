31921c2744b04945d2b0376ef688e6528fda9500
SanjanaKumarasingha <Sanjanakumarasingha1@gmail.com>
2024-07-10 21:26:40 +0530

    error fix

--- backend/src/api/v1/routes/chat.routes.js
+++ backend/src/api/v1/routes/chat.routes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // define the routes related to the chat feature
 router.post('/:id', businessMessageHandler);
 router.delete('/:id/message/:user_id', businessMessageHandler);
-router.post('/id/message/:user_id', businessMessageHandler);
+router.post('/:id/message/:user_id', businessMessageHandler);
 router.get('/:id', businessMessageHandler);
 
 module.exports = router;
