f9977299958e5fc7bfdc9d66ae2f0f36c1684cec
Verosha Kriyanjala <veroshakriyanjala32@gmail.com>
2024-07-10 20:13:25 +0530

    normal_user_profile_update and normal_user_profile delete

--- backend/src/api/v1/start/routes.js
+++ backend/src/api/v1/start/routes.js
@@ -1,5 +1,7 @@
 const RegisterRouter = require('../routes/register.routes');
+const ProfileRouter = require('../routes/profile.routes');
 
 module.exports = (app) => {
   app.use('/api/v1/register', RegisterRouter);
+  app.use('/api/v1/profile', ProfileRouter);
 };
